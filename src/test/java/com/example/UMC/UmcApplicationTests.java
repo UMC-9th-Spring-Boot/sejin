5d4f40aebe6560379fc45eaae06bc7bfd53b75f9
Kimsejin-56 <saj5877@naver.com>
2025-10-07 15:44:26 +0900

    first Commit

---
+++ src/test/java/com/example/UMC/UmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
